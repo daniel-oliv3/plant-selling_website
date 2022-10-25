e8eadcf066c2ffa85b09bbd8c47b3aba07184835
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-25 14:52:21 -0300

    Home-slider

--- js/script.js
+++ js/script.js
@@ -20,8 +20,18 @@ window.onscroll = () => {
     }
 }
 
-
-
+/* Swiper-slider */
+var swiper = new Swiper(".home-slider", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop:true,
+});
 
 
 
