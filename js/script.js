fd4ea2e970f6fb247985508092f3161a114f14a3
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-25 10:47:53 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*Menu toggle*/
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-3');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
@@ -11,6 +12,12 @@ menu.addEventListener('click', () => {
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    if(window.scrollY > 250){
+        header.classList.add('active');
+    }else {
+        header.classList.remove('active');
+    }
 }
 
 
